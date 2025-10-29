bd648117fbbf9feb6caa4d8f49705a9fc6830697
gitaloisiohub <aloisiojuniorprimeiro@gmail.com>
2025-10-29 01:00:58 +0000

    Desafio do Xadrez Nivel Mestre!

--- xadrez.c
+++ xadrez.c
@@ -48,9 +48,101 @@ int main() {
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
 
+   printf("Movimento do Cavalo:\n");
+
+   
+    const int casasBaixo = 2;
+    const int casasEsquerda = 1;
+
+
+    for (int i = 0; i < casasBaixo; i++) {
+        printf("Baixo\n");
+    }
+
+    int k = 0;
+    while (k < casasEsquerda) {
+        printf("Esquerda\n");
+        k++;
+    }
+
+
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+
+    void moverTorre(int casasRestantes) {
+    if (casasRestantes <= 0) {
+        return;
+    }
+    printf("Cima\n");
+    moverTorre(casasRestantes - 1);
+
+}
+
+    void moverBispo(int casasRestantes) {
+    if (casasRestantes <= 0) {
+        return;
+    }
+
+    for (int vertical = 0; vertical < 1; vertical++) {
+        for (int horizontal = 0; horizontal < 1; horizontal++) {
+            printf("Diagonal superior direita\n");
+        }
+    }
+
+    void moverRainha(int casasRestantes) {
+    if (casasRestantes <= 0) {
+        return;
+    }
+    printf("Direita\n");
+    moverRainha(casasRestantes - 1);
+}
+
+    void moverCavalo() {
+    const int casasCima = 2;
+    const int casasDireita = 1;
+
+    printf("Movimento do Cavalo:\n");
+
+    for (int i = 1, j = 0; i <= casasCima; i++, j++) {
+        printf("Cima\n");
+
+        if (i < casasCima) {
+            continue;
+        }
+
+        int k = 0;
+        while (k < casasDireita) {
+            printf("Direita\n");
+            k++;
+
+            
+            if (k == casasDireita) {
+                break;
+            }
+        }
+    }
+}
+
+    int main() {
+
+    const int casasTorre = 3;
+    const int casasBispo = 3;
+    const int casasRainha = 3;
+
+    printf("Movimento da Torre:\n");
+    moverTorre(casasTorre);
+
+    printf("\nMovimento do Bispo:\n");
+    moverBispo(casasBispo);
+
+
+    printf("\nMovimento da Rainha:\n");
+    moverRainha(casasRainha);
+
+
+    printf("\n");
+    moverCavalo();
 
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
